f430c31b0cde186c1402fde8bd2e303e4a2efd8f
Edilio NUNES DUARTE JUNIOR <84047076+ediliojr@users.noreply.github.com>
2023-03-24 08:19:37 -0300

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,7 +6,7 @@ export class Home extends Component {
             <div  >
                 <h1>Bem vindo a pagina de cadastro</h1>
             
-                <img src="https://cdn.myportfolio.com/846d6150937d91a5d929de24a4585d11/d31ac7c2-fdaa-4ac4-830b-23cf50d54901.png?h=8535b777d6c660c342ecc7e071f6d1cd" width="80%" height= "50%"></img>
+                <img src="https://blog.vantagecircle.com/content/images/size/w1000/2020/07/welcome-messages-for-employees.png" width="80%" height= "50%"></img>
           
             </div>
         )
